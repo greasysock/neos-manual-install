c3303fb07940eb76ee92ab7b9f62dce1c22f1693
dogtreatfairy <98328354+dogtreatfairy@users.noreply.github.com>
2024-03-06 18:55:01 -0600

    Update c2setup.sh
    clone dragonpilot to openpilot

--- c2setup.sh
+++ c2setup.sh
@@ -8,10 +8,10 @@ DEFAULT_GITHUB_BRANCH="r2"
 cd /data
 rm -rf openpilot
 rm -rf dragonpilot
-time git clone https://github.com/${1:-$DEFAULT_GITHUB_OWNER}/dragonpilot.git -b ${2:-$DEFAULT_GITHUB_BRANCH} --recurse-submodules --depth 1
+time git clone https://github.com/${1:-$DEFAULT_GITHUB_OWNER}/dragonpilot.git -b ${2:-$DEFAULT_GITHUB_BRANCH} --recurse-submodules --depth 1 openpilot
 
 cd /data/data/com.termux/files
 echo $'#!/usr/bin/bash\n\ncd /data/openpilot\n./launch_openpilot.sh\n' > continue.sh
 chmod +x continue.sh
-echo "Install complete, rebooting..."
+echo "Installation complete. Rebooting..."
 reboot
