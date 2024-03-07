8bbe313f8d769ee315cc0848b357265413f17b92
dogtreatfairy <98328354+dogtreatfairy@users.noreply.github.com>
2024-03-06 19:07:39 -0600

    Update c2setup.sh
    Added menu for install options

--- c2setup.sh
+++ c2setup.sh
@@ -5,10 +5,37 @@ set -e
 DEFAULT_GITHUB_OWNER="dragonpilot-community"
 DEFAULT_GITHUB_BRANCH="r2"
 
+echo "Select an option:"
+echo "1. Dragon Pilot - R2"
+echo "2. Dragon Pilot - Deprecated-Release2 (dogtreatfairy)"
+echo "3. Open Pilot - 0.8.13 (Stock)"
+read -p "Enter your choice (1/2/3): " OPTION
+
+case $OPTION in
+    1)
+        GITHUB_USER="dragonpilot-community"
+        BRANCH="r2"
+        REPO="dragonpilot.git"
+        ;;
+    2)
+        GITHUB_USER="dogtreatfairy"
+        BRANCH="deprecated-release2"
+        REPO="dragonpilot.git"
+        ;;
+    3)
+        GITHUB_USER="commaai"
+        BRANCH="release2"
+        REPO="openpilot.git"
+        ;;
+    *)
+        echo "Invalid option. Exiting."
+        exit 1
+        ;;
+esac
+
 cd /data
 rm -rf openpilot
-rm -rf dragonpilot
-time git clone https://github.com/${1:-$DEFAULT_GITHUB_OWNER}/dragonpilot.git -b ${2:-$DEFAULT_GITHUB_BRANCH} --recurse-submodules --depth 1 openpilot
+time git clone https://github.com/$GITHUB_USER/$REPO -b $BRANCH --recurse-submodules --depth 1 openpilot
 
 cd /data/data/com.termux/files
 echo $'#!/usr/bin/bash\n\ncd /data/openpilot\n./launch_openpilot.sh\n' > continue.sh
